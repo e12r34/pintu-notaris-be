3a1fe8c0cce0f15f8b288737f7a1fbb479bdbd7f
e12r34 <moefathul@gmail.com>
2023-10-15 00:07:12 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   const config = new DocumentBuilder()
   .setTitle('API Cuti dan Kondite')
   .setDescription('Dokumentasi API untuk endpoint Cuti dan Kondite')
