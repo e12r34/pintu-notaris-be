afe686762914a702b1e19c8cd21110fd6f0a426a
e12r34 <moefathul@gmail.com>
2023-10-18 14:16:20 +0700

    ADD: CONFIG LIMIT JSON AND URLENCODED

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +19,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
-  
+  app.use(express.json({ limit: process.env.APP_LIMIT_JSON }));
+  app.use(express.urlencoded({ extended: true, limit: process.env.APP_LIMIT_URL_ENCODED }));
+
   await app.listen(3000);
 }
 bootstrap();
